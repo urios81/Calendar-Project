c837aeff652225480d76ad3c48f4384fd50499cc
Uziel Rios <82281575+urios81@users.noreply.github.com>
2024-08-28 23:36:43 -0500

    Update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 <script type="text/javascript" id="Calendar_script">
     (function() {
